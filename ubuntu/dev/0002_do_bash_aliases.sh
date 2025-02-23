e6cb712f27bf920a24a098de31fb8384e45d0d20
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-02-23 18:50:37 -0500

    added color back to ls -la

--- ubuntu/dev/0002_do_bash_aliases.sh
+++ ubuntu/dev/0002_do_bash_aliases.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Define bash aliases
-cat <<'EOF' >> ~/.bash_aliases
-alias l='ls -la'
+cat <<'EOF' >>~/.bash_aliases
+alias l='ls -la --color=auto'
 EOF
 
 # Ensure .bash_aliases is sourced in .bashrc
 if ! grep -q "if \[ -f ~/.bash_aliases \]; then" ~/.bashrc; then
-  cat <<'EOF' >> ~/.bashrc
+  cat <<'EOF' >>~/.bashrc
 
 # Source custom aliases if available
 if [ -f ~/.bash_aliases ]; then
