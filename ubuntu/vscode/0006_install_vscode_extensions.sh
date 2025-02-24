5eff3e6be67df46eb72a8d469a9475dc809f9ea9
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-02-23 19:08:25 -0500

    added python language support extensions

--- ubuntu/vscode/0006_install_vscode_extensions.sh
+++ ubuntu/vscode/0006_install_vscode_extensions.sh
@@ -5,9 +5,15 @@ extensions=(
     # Formatting
     "esbenp.prettier-vscode"
     "foxundermoon.shell-format"
+    "ms-python.black-formatter"
 
     # Github
     "eamodio.gitlens"
+
+    # Languages
+    "ms-python.python"
+    "ms-python.vscode-pylance"
+
 )
 
 for extension in "${extensions[@]}"; do
